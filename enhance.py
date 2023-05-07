e4c9f08fb7058fe064df21d89369e76819f2d63f
Neoon <neo@tiefkuehler-madness.me>
2023-05-08 01:26:30 +0200

    update

--- enhance.py
+++ enhance.py
@@ -25,26 +25,37 @@ with open('asn.dat') as file:
         ips.append(subnet.split("/")[0])
         sub[subnet.split("/")[0]] = subnet
 
+def resolve(ip):
+    try:    
+        target = readers[targetDB].city(ip)
+    except:
+        return False,False
+    try:
+        verify = readers[verifyDB].city(ip)
+    except:
+        return target,False
+    return target,verify
+
 readers = {verifyDB:geoip2.database.Reader(verifyDB),targetDB:geoip2.database.Reader(targetDB)}
-results = {"match":0,"correction":0,"fail":0}
+results = {"match":0,"correction":0,"fail":0,"unable":0}
 export = {}
 for ip in ips:
-    try:
-        target = readers[targetDB].city(ip)
-        verify = readers[verifyDB].city(ip)
+    target,verify = resolve(ip)
+    if target and not f"{target.location.latitude},{target.location.longitude}" in export: export[f"{target.location.latitude},{target.location.longitude}"] = []
+    if verify and not f"{verify.location.latitude},{verify.location.longitude}" in export: export[f"{verify.location.latitude},{verify.location.longitude}"] = []
+    if target and verify:
         if target.continent.code in ["EU","NA","AS"]:
-            if not f"{target.location.latitude},{target.location.longitude}" in export: export[f"{target.location.latitude},{target.location.longitude}"] = []
-            if not f"{verify.location.latitude},{verify.location.longitude}" in export: export[f"{verify.location.latitude},{verify.location.longitude}"] = []
             if target.continent.code == verify.continent.code:
                 results["match"] += 1
                 export[f"{target.location.latitude},{target.location.longitude}"].append(sub[ip])
             else:
                 results["correction"] += 1
                 export[f"{verify.location.latitude},{verify.location.longitude}"].append(sub[ip])
-    except Exception as e:
+    elif target and verify is False:
+        export[f"{target.location.latitude},{target.location.longitude}"].append(sub[ip])
+        results["unable"] += 1
+    else:
         results["fail"] += 1
-        print(e)
-        continue
 
 writer = MMDBWriter(4, 'GeoIP2-City', languages=['EN'], description="Mah own .mmdb")
 for location,subnets in export.items():
